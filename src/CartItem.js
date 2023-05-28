1cc7eac5eaf2015ab0117e15ab55fac92f1c15a9
anmolalgorist7110 <anmolalgo12345@gmail.com>
2023-05-28 14:35:14 +0530

    created styling for the cart items

---
+++ src/CartItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//inherting the from the Component which is in the React package
+ class CartItem extends React.Component {
+    render(){
+        return(
+         <div></div>
+        );
+    }
+ }

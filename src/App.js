a95c25ce750a38f5b368d7b3a95afac3b378b3fa
anmolalgorist7110 <anmolalgo12345@gmail.com>
2023-05-28 15:47:37 +0530

    added an image block

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
       <h1>My Cart</h1>
+      <CartItem/>
     </div>
   );
 }
+ 
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -4,7 +4,29 @@ import React from "react";
  class CartItem extends React.Component {
     render(){
         return(
-         <div></div>
+         <div className="cart-item">
+            <div className="left-block">
+             <img style={styles.image} alt=""/>
+            </div>
+            <div className="right-block">
+             <div>Phone</div>
+             <div>Rs 1999</div>
+             <div>Qty : 1</div>
+             <div className="cart-item-actions">
+              {/* {buttons} */}
+             </div>
+            </div>
+         </div>
         );
     }
  }
+
+ const styles = {
+    image:{
+        height: 110,
+        width: 110,
+        borderRadius: 4
+    }
+ }
+
+ export default CartItem ;
